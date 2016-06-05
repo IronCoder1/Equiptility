c33005b7804cf994c763971a290bbb18120e7405
Anthony Olu <b.ioanth1000@gmail.com>
2016-06-05 23:05:53 +0100

    Initial Commit

---
+++ EquiptiLity/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  EquiptiLity
+//
+//  Created by MBPinTheAir on 05/06/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
